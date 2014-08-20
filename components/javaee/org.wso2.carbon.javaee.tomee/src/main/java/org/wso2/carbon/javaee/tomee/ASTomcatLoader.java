0c35c40d00ce1c867726bb3cdd31541f9e5af769
Kasun Gajasinghe <kasung@wso2.com>
2014-08-20 19:33:51 +0530

    Since we initialize TomcatWebAppBuilder early, we need to set the jdbc pool early. TWAB does some heavy operations which require the jdbc pool.

--- components/javaee/org.wso2.carbon.javaee.tomee/src/main/java/org/wso2/carbon/javaee/tomee/ASTomcatLoader.java
+++ components/javaee/org.wso2.carbon.javaee.tomee/src/main/java/org/wso2/carbon/javaee/tomee/ASTomcatLoader.java
@@ -16,6 +16,7 @@
 package org.wso2.carbon.javaee.tomee;
 
 import org.apache.openejb.assembler.classic.WebAppBuilder;
+import org.apache.openejb.config.ConfigurationFactory;
 import org.apache.openejb.loader.SystemInstance;
 import org.apache.tomee.catalina.TomcatLoader;
 import org.apache.tomee.catalina.TomcatWebAppBuilder;
@@ -25,6 +26,12 @@ import java.util.Properties;
 public class ASTomcatLoader extends TomcatLoader {
 
     public void initialize(Properties properties) throws Exception {
+
+        //Since we initialize TomcatWebAppBuilder early, we need to set the jdbc pool here.
+        //TWAB does some heavy operations which require the jdbc pool.
+        setJdbcPool();
+
+        //Set our own TWAB. Why we need this? To set our own Tomcat context LifeCycleListener
         TomcatWebAppBuilder tomcatWebAppBuilder = (TomcatWebAppBuilder)
                 SystemInstance.get().getComponent(WebAppBuilder.class);
         if (tomcatWebAppBuilder == null) {
@@ -33,5 +40,21 @@ public class ASTomcatLoader extends TomcatLoader {
             SystemInstance.get().setComponent(WebAppBuilder.class, tomcatWebAppBuilder);
         }
         super.initialize(properties);
+    }
+
+    /**
+     * Snippet taken from TomcatWebAppBuilder#initialize method.
+     */
+    protected void setJdbcPool() {
+        // set tomcat pool
+        try {// in embedded mode we can easily remove it so check we can use it before setting it
+            final Class<?> creatorClass = TomcatLoader.class.getClassLoader().loadClass("org.apache.tomee.jdbc.TomEEDataSourceCreator");
+            SystemInstance.get().setProperty(ConfigurationFactory.OPENEJB_JDBC_DATASOURCE_CREATOR, creatorClass.getName());
+        } catch (Throwable ignored) {
+            // will use the defaul tone
+        }
+
+
+
     }
 }
