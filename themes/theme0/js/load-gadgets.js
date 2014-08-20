d0713acb90ba80c1bce78c8ba4a48bfe9478918a
Manu Perera <manu@wso2.com>
2014-08-20 19:45:21 +0530

    render all gadgets.

--- themes/theme0/js/load-gadgets.js
+++ themes/theme0/js/load-gadgets.js
@@ -1,6 +1,11 @@
 $(function(){
-    var gadgetUrl = config.gadgetsUrlBase + '/stacked-line-chart/stacked-line-chart.xml';
 //    var opt = {prefs: {dataSource: caramel.context + '/gadgets/stacked-line-chart/datasource/dataFile1.jag'} };
-    var opt = {prefs: {dataSource: caramel.context + '/api/as-data.jag'} };
-    UESContainer.renderGadget('request-graph', gadgetUrl, opt);
+    var gadgetUrl = config.gadgetsUrlBase + '/stacked-line-chart/stacked-line-chart.xml';
+    var opt;
+    var gadgetTypes = {'request-graph': 'request', 'response-graph': 'response', 'error-graph': 'error'};
+    for (var gadgetElmId in gadgetTypes) {
+        opt = {prefs: {dataSource: caramel.context + '/api/as-data.jag'} };
+        opt.prefs.appStatType = gadgetTypes[gadgetElmId];
+        UESContainer.renderGadget(gadgetElmId, gadgetUrl, opt);
+    }
 });
