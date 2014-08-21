a329d0ecc9846cecf0ca1be757e2e1f20393268d
Manu Perera <manu@wso2.com>
2014-08-21 17:31:09 +0530

    duplicating gadget to be used as a base for new info-box gadget

--- themes/theme0/js/load-gadgets.js
+++ themes/theme0/js/load-gadgets.js
@@ -1,8 +1,8 @@
 $(function(){
-    var gadgetUrl = config.gadgetsUrlBase + '/stacked-line-chart/stacked-line-chart.xml';
-    var opt;
+    var opt, gadgetUrl;
     $('.gadget-holder').each(function (i, el) {
         var $el = $(el);
+        gadgetUrl = config.gadgetsUrlBase + '/' + $el.attr('data-gadget') + '/' + $el.attr('data-gadget') + '.xml';
         opt = {prefs: {dataSource: caramel.context + '/api/as-data.jag'} };
         opt.prefs.appStatType = $el.attr('data-type');
         UESContainer.renderGadget($el.attr('id'), gadgetUrl, opt);
