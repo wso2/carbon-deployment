f5248b85286b4b4e9decc77e9f55eb4e43b345a4
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-08-22 13:19:37 +0000

    added few

--- themes/theme0/ui/js/custom.js
+++ themes/theme0/ui/js/custom.js
@@ -11,14 +11,6 @@ jQuery(document).ready(function() {
 	//date picker
 	jQuery('#reportrange').daterangepicker(
     {
-      ranges: {
-         'Today': [moment(), moment()],
-         'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-         'Last 7 Days': [moment().subtract('days', 6), moment()],
-         'Last 30 Days': [moment().subtract('days', 29), moment()],
-         'This Month': [moment().startOf('month'), moment().endOf('month')],
-         'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-      },
       startDate: moment().subtract('days', 29),
       endDate: moment()
     },
