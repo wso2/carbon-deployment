c2a8e7005b067d2f1ab418b293623279dfaed17f
Manu Perera <manu@wso2.com>
2014-08-25 19:50:18 +0530

    making gadgets render at tab switch.

--- themes/theme0/js/load-gadgets.js
+++ themes/theme0/js/load-gadgets.js
@@ -1,11 +1,21 @@
-$(function(){
+var renderAll = function(){
     var opt, gadgetUrl;
     $('.gadget-holder').each(function (i, el) {
         var $el = $(el);
-        gadgetUrl = config.gadgetsUrlBase + '/' + $el.attr('data-gadget') + '/' + $el.attr('data-gadget') + '.xml';
-        opt = {prefs: {dataSource: caramel.context + '/api/as-data.jag'} };
-        opt.prefs.appStatType = $el.attr('data-type');
-        opt.prefs.appname = appname || '';
-        UESContainer.renderGadget($el.attr('id'), gadgetUrl, opt);
+        if($el.is(':visible') && !Boolean($el.attr('data-rendered'))){
+            gadgetUrl = config.gadgetsUrlBase + '/' + $el.attr('data-gadget') + '/' + $el.attr('data-gadget') + '.xml';
+            opt = {prefs: {dataSource: caramel.context + '/api/as-data.jag'} };
+            opt.prefs.appStatType = $el.attr('data-type');
+            opt.prefs.appname = appname || '';
+            UESContainer.renderGadget($el.attr('id'), gadgetUrl, opt);
+            $el.attr('data-rendered',true);
+        }
     });
+};
+
+$(function(){
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        renderAll();
+    });
+    renderAll();
 });
