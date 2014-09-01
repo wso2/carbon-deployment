cd0c2a0876627121c76e72eec053d9eac4614fa9
Kalpa Welivitigoda <callkalpa@gmail.com>
2014-09-01 16:57:37 +0530

    implemented third level pages for browser, operating system and device type

---
+++ themes/theme0/renderers/browser.js
@@ -0,0 +1,66 @@
+var render = function (theme, data, meta, require) {
+
+    if(data.error.length == 0 ){
+        theme('index', {
+            config: [{
+                context: {
+                    gadgetsUrlBase: data.config.gadgetsUrlBase
+                }
+            }],
+            title: [{
+                context:{
+                    page_title:'AS Dashboard'
+                }
+            }],
+            appname: [{
+                context:data.appname
+            }],
+            left_side:[
+                {
+                    partial: 'left_side',
+                    context: {
+                        user_name: 'dakshika@wso2.com ',
+                        user_avatar:'dakshika',
+                        breadcrumb:'Service Cluster System Statistics'
+                    }
+                }
+            ],
+            right_side: [
+
+                {
+                    partial: 'browser',
+                    context:{
+                        aspect:'Browser' ,
+                        appname : data.appname,
+                        user_name: 'dakshika@wso2.com ',
+                        user_avatar:'dakshika',
+                        data:  data.panels,
+                        updateInterval: data.updateInterval
+                    }
+                }
+            ]
+        });
+
+    }else{
+
+        theme('index', {
+            title: [
+
+            ],
+            header:[
+                {
+                    partial: 'header_login'
+                }
+            ],
+            body: [
+
+                {
+                    partial: 'error',
+                    context:{
+                        error:  data.error
+                    }
+                }
+            ]
+        });
+    }
+};
