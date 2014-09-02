0b43da228914fbb73178f127cbe95223f619ba8a
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2014-09-02 06:38:19 +0000

    added application table bootstrap support

--- themes/theme0/helpers/aggregated-index.js
+++ themes/theme0/helpers/aggregated-index.js
@@ -1,6 +1,6 @@
 var resources = function (page, meta) {
     return {
-        js: ['load-app-table.js'],
-        css: []
+        js: ['load-app-table.js', 'dataTables.bootstrap.js'],
+        css: ['dataTables.bootstrap.css']
     };
 };
