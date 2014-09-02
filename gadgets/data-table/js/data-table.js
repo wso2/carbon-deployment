52741dba5129ad06fd65d13a4285692343a92b40
Kalpa Welivitigoda <callkalpa@gmail.com>
2014-09-02 21:59:03 +0530

    reused data tables gadget for the app list table in the home page

--- gadgets/data-table/js/data-table.js
+++ gadgets/data-table/js/data-table.js
@@ -27,19 +27,67 @@ function onDataReceived(data) {
     var tableData = data.data;
     var tableHeadings = data.headings;
     var orderColumn = data.orderColumn;
-    var headings = [];
-
-    for(var i = 0; i < tableHeadings.length; i++){
-        headings.push({"title": tableHeadings[i]});
-    }
+    var headings = "<thead><tr>";
+    var rowSpan = 1;
+    var applist = data.applist || undefined;
 
     $('#placeholder').html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="table" style="width: 100%"></table>');
 
-    $('#table').dataTable({
+    for (var i = 0; i < tableHeadings.length; i++) {
+        if (tableHeadings[i] instanceof Object) {
+            rowSpan = 2;
+            break;
+        }
+    }
+
+    for (var i = 0; i < tableHeadings.length; i++) {
+        if (typeof(tableHeadings[i]) == "string") {
+            headings += "<th rowspan='" + rowSpan + "'>";
+            headings += tableHeadings[i];
+        } else {
+            headings += "<th colspan='" + tableHeadings[i]["sub"].length + "'>";
+            headings += tableHeadings[i]["parent"];
+        }
+        headings += "</th>";
+    }
+
+    headings += "</tr>";
+
+    if (rowSpan > 1) {
+        headings += "<tr>";
+        for (var i = 0; i < tableHeadings.length; i++) {
+            if (tableHeadings[i] instanceof Object) {
+                var subHeadings = tableHeadings[i]["sub"];
+                for (var j = 0; j < subHeadings.length; j++) {
+                    headings += "<th>" + subHeadings[j] + "</th>"
+                }
+            }
+        }
+        headings += "</tr>";
+    }
+
+    headings += "</thead>";
+
+    $("#table").html(headings);
+
+    var table = $('#table').dataTable({
         "data": tableData,
-        "order": [orderColumn],
-        "columns" : headings
+        "order": [orderColumn]
     });
+
+    if(applist){
+        $('#table tbody').on('click', 'tr', function(){
+            if($(this).hasClass('selected')){
+                $(this).removeClass('selected');
+            } else{
+                var webapp = table.fnGetData(this)[0];
+                table.$('tr.selected').removeClass('selected');
+                $(this).addClass('selected');
+                var webappUrl = parent.window.location.origin + parent.window.location.pathname + 'webapps/' + webapp + '/';
+                parent.window.location.href = webappUrl;
+            }
+        });
+    }
 }
 
 $(document).ready(function () {
