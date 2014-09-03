30879504292d4919da39b58992c432c52bc5af5f
Kalpa Welivitigoda <callkalpa@gmail.com>
2014-09-03 11:37:39 +0530

    passing node selection to gadgets

--- gadgets/location/js/location.js
+++ gadgets/location/js/location.js
@@ -1,14 +1,18 @@
 var pref = new gadgets.Prefs();
 var values = null;
+var node = pref.getString("node") || undefined;
+var start = pref.getString("startTime") || undefined;
+var end = pref.getString("endTime") || undefined;
 
 var url = pref.getString("dataSource");
 
-function fetchData(startTime, endTime) {
+function fetchData() {
     var url = pref.getString("dataSource");
 
     var data = {
-        start_time: startTime,
-        end_time: endTime,
+        start_time: start,
+        end_time: end,
+        node: node,
         action: pref.getString("appStatType")
     };
     var appname = pref.getString("appname");
@@ -38,7 +42,16 @@ gadgets.HubSettings.onConnect = function () {
 
     gadgets.Hub.subscribe('wso2.gadgets.charts.timeRangeChange',
         function (topic, data, subscriberData) {
-            fetchData(data.start.format('YYYY-MM-DD HH:mm'), data.end.format('YYYY-MM-DD HH:mm'))
+            start = data.start.format('YYYY-MM-DD HH:mm');
+            end = data.end.format('YYYY-MM-DD HH:mm');
+            fetchData();
+        }
+    );
+
+    gadgets.Hub.subscribe('wso2.gadgets.charts.ipChange',
+        function (topic, data, subscriberData) {
+            node = data;
+            fetchData();
         }
     );
 };
