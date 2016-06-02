bfbd2ffd7714ff7824dd46bd1db1fedf58155881
Manuri <amaya.uom@gmail.com>
2016-06-02 14:01:13 +0530

    Removing unnecessary new lines getPagedWebapps of WebappAdmin

--- components/webapp-mgt/org.wso2.carbon.webapp.mgt/src/main/java/org/wso2/carbon/webapp/mgt/WebappAdmin.java
+++ components/webapp-mgt/org.wso2.carbon.webapp.mgt/src/main/java/org/wso2/carbon/webapp/mgt/WebappAdmin.java
@@ -296,7 +296,6 @@ public class WebappAdmin extends AbstractAdmin {
             httpProxyPort = Integer.parseInt(httpProxyPortString);
             webappsWrapper.setHttpPort(httpProxyPort);
         }
-
         if (httpProxyPort == -1) {
             if (getConfigContext().getAxisConfiguration().getTransportIn("http") != null) {
                 httpProxyPort = CarbonUtils.getTransportProxyPort(getConfigContext(), "http");
@@ -311,12 +310,10 @@ public class WebappAdmin extends AbstractAdmin {
 
         String httpsProxyPortString = serverConfiguration.getFirstProperty("Ports.WorkerHttpsProxyPort");
         int httpsProxyPort = -1;
-
         if (httpsProxyPortString != null) {
             httpsProxyPort = Integer.parseInt(httpsProxyPortString);
             webappsWrapper.setHttpsPort(httpsProxyPort);
         }
-
         if (httpsProxyPort == -1) {
             if (getConfigContext().getAxisConfiguration().getTransportIn("https") != null) {
                 httpsProxyPort = CarbonUtils.getTransportProxyPort(getConfigContext(), "https");
