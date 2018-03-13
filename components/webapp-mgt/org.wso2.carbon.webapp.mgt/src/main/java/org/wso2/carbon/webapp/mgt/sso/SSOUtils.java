36690aa4cd28c3dac7169f278e94fefe8b010b92
hastef88 <hastef.ds@gmail.com>
2018-03-13 09:34:32 -0700

    Allow to override ACS url and SSO Skip URIs
    This fix allows
    
    1. The ACS uri to be overrriden if there is an HTTP header present in the request (where the header name is equal to configuration "CustomACSHeader" in the AS_HOME/repository/conf/security/sso-sp-config.properties file.)
    2. The SSO Skip URIs to be overridden if there is a context param "sso.skip.uris" present in the webapp web.xml.
    3. To replace the tenant URI component (/t/tenant.com/webapps) from the redirect URIs set within the valve.

--- components/webapp-mgt/org.wso2.carbon.webapp.mgt/src/main/java/org/wso2/carbon/webapp/mgt/sso/SSOUtils.java
+++ components/webapp-mgt/org.wso2.carbon.webapp.mgt/src/main/java/org/wso2/carbon/webapp/mgt/sso/SSOUtils.java
@@ -18,6 +18,8 @@
 
 package org.wso2.carbon.webapp.mgt.sso;
 
+import org.apache.catalina.connector.Request;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -27,6 +29,8 @@ import java.util.Properties;
 public class SSOUtils {
 
     private static Log log = LogFactory.getLog(SSOUtils.class);
+
+
 
     public static boolean isSSOSPConfigExists() {
         File configFile = new File(WebappSSOConstants.SSO_SP_CONFIG_PATH);
@@ -41,13 +45,55 @@ public class SSOUtils {
         return issuerId;
     }
 
-    public static String generateConsumerUrl(String contextPath, Properties ssoSPConfigProperties) {
-        return ssoSPConfigProperties.getProperty(WebappSSOConstants.APP_SERVER_URL) + contextPath +
-                ssoSPConfigProperties.getProperty(WebappSSOConstants.CONSUMER_URL_POSTFIX);
-    }
-
     public static String getSAMLSSOURLforApp(String requestURI, Properties ssoSPConfigProperties) {
         return ssoSPConfigProperties.getProperty(WebappSSOConstants.APP_SERVER_URL) + requestURI +
                 ssoSPConfigProperties.getProperty(WebappSSOConstants.SAMLSSOURL);
+    }
+
+    /**
+     * By default this method generates the ACS url based on the application server hostname + tenant.suffix. But, in
+     * case the request contains a custom HTTP header containing a different ACS url, this method will honor that url.
+     * (This kind of a scenario can happen when the end user is hoping to access a web application with a custom
+     * domain and URI through a router like Nginx.)
+     * @param request request object
+     * @param ssoSPConfigProperties SSO properties loaded from server level sso-sp-config.properties file.
+     * @return appropriate ACS url
+     */
+    static String generateConsumerUrl(Request request, Properties ssoSPConfigProperties) {
+
+        String assertionConsumerURL = request.getHeader(ssoSPConfigProperties.getProperty(WebappSSOConstants
+                .CUSTOM_ACS_HEADER));
+
+        if (StringUtils.isBlank(assertionConsumerURL)) {
+            assertionConsumerURL = ssoSPConfigProperties.getProperty(WebappSSOConstants.APP_SERVER_URL) + request.getContextPath() +
+                    ssoSPConfigProperties.getProperty(WebappSSOConstants.CONSUMER_URL_POSTFIX);
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("Setting ACS_URL to : " + assertionConsumerURL);
+        }
+
+        return assertionConsumerURL;
+    }
+
+    /**
+     * In case an end user wishes to access a web application from a custom domain without any tenant awareness by
+     * fronting the application server with a router like Nginx, we need to detect such a scenario and remove the
+     * tenant component from all redirect URIs during the SSO flow.
+     * @param uri original URI path
+     * @param customACSUrl a custom HTTP header containing a different ACS url
+     * @param tenantName Name of tenant that owns the web application.
+     * @return
+     */
+    static String removeTenantFromURI(String uri, String customACSUrl, String tenantName) {
+
+        String returnURI = uri;
+
+        if (!StringUtils.isBlank(customACSUrl) && !StringUtils.isBlank(tenantName) && !customACSUrl.contains(tenantName)) {
+            returnURI = returnURI.replace(WebappSSOConstants.TENANT_URL_PREFIX + tenantName +
+                    WebappSSOConstants.WEBAPP_PREFIX,"");
+        }
+
+        return returnURI;
     }
 }
